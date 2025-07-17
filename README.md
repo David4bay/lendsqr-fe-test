d94d1de67c158d60496702e857a93c3c218c4f3b
David Bayode <david4bay@gmail.com>
2025-07-17 12:47:19 +0100

    building context for user data and login page, default jsx in App.tsx removed.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Welcome to my solution to the Lendsqr Lender-as-a-Service Frontend Assessment. ð
 
 - Typed React
 - Vite bundler/build tool
+- SCSS and CSS
 
 ### Getting Started
 To get started with this project, clone the repo.
---
+++ lendsqr-fe-test/src/context/UserContext.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from "react";
+import { createContext } from "react";
+
+export const userLoggedInStatus = createContext("")
+
+interface Props {
+    children: React.ReactNode
+}
+
+export default function UserContext({ children }: Props) {
+
+    return (
+        <React.Fragment>
+            {children}
+        </React.Fragment>
+    )
+}
