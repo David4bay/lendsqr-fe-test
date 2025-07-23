7a51fbcbf85c7b25365eb3f9f3a3234418566827
David Bayode <david4bay@gmail.com>
2025-07-23 17:33:07 +0100

    completed single user dashboard

--- lendsqr-fe-test/src/components/UserProfile.tsx
+++ lendsqr-fe-test/src/components/UserProfile.tsx
@@ -22,277 +22,286 @@ function UserProfile() {
 
     return (
         <>
-        <article>
-        <div>
-            <div>
-                <div>
-                    <Link to="/users">
+        <article className="single__user">
+        <div className="single__user-container user__action">
+            <div className="user__action-container navigate">
+                <div className="navigate__back">
+                    <Link to="/users" className="navigate__text">
                         <img src="/back-logo.png" alt="go back to previous page icon" />
                         <span className="">
                         Back to Users
                         </span>
                     </Link>
                 </div>
-                <div>
-                    <h3>User Details</h3>
-                </div>
-                <div>
-                    <div>
-                        <button>
-                            Blacklist User
-                        </button>
-                        <button>
-                            Activate User
-                        </button>
-                    </div>
+                <div className="user__action__container">
+                    <h3 className="user__action__container-title">User Details</h3>
+                        <div className="actions">
+                            <button className="blacklisted small-blacklist">
+                                Blacklist User
+                            </button>
+                            <button className="active small-activate">
+                                Activate User
+                            </button>
+                        </div>
                 </div>
             </div>
             <div>
-                <div>
-                    <div>
-                        <div>
-                            <img src="" alt="" />
-                            <h3>{data.username}</h3>
-                            <p>{data._id}</p>
+                <div className="top__header">
+                    <div className="header">
+                        <div className="header__section-avatar">
+                            <div className="avatar">
+                                    <img src="/avatar.png" alt="user avatar icon" className="avatar__image" />
+                                <div className="avatar__details">
+                                    <h3 className="avatar__details-title">
+                                        {data.fullname}
+                                    </h3>
+                                    <p className="avatar__details-id">{data._id.slice(0, 11)}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="header__section-tier">
+                            <span className="tier__title">User's Tier</span>
+                            <div className="tier__ratings">
+                            {Array.from({length: 3}, (_, i) => i + 1).map((userTier) => {
+                                if (data.tier < userTier) {
+                                    return (
+                                        <>
+                                            <img src="/unfilled-star.png" alt="user's tier icon" />
+                                        </>
+                                    )
+                                } else {
+                                    return (
+                                        <img src="/filled-star.png" alt="user's tier icon" />
+                                    )
+                                }
+                            })}
+                            </div>
+                        </div>
+                        <div className="header__section-income">
+                            <h3 className="income__amount">₦{data.monthly_income_2.toLocaleString("en-NG")}</h3>
+                            <p className="income__bank">
+                                {data.bank_account}/{data.bank}
+                            </p>
                         </div>
                     </div>
-                    <div>
-                        <span className="">User's Tier</span>
-                        {Array.from({length: 3}, (_, i) => i + 1).map((userTier) => {
-                            if (data.tier < userTier) {
-                                return (
-                                    <>
-                                        <img src="/unfilled-star.png" alt="user's tier icon" />
-                                    </>
-                                )
-                            } else {
-                                return (
-                                    <img src="/filled-star.png" alt="user's tier icon" />
-                                )
-                            }
-                        })}
-                    </div>
-                    <div>
-                        <h3>{data.monthly_income}</h3>
-                        <p>
-                            {data.bank_account}/{data.bank}
-                        </p>
-                    </div>
-                </div>
-            </div>
             <div>
-                <ul>
-                    <li>
+                <ul className="header__nav">
+                    <li className="header__nav-item">
                         General Details 
                     </li>
-                    <li>
+                    <li className="header__nav-item">
                         Documents 
                     </li>
-                    <li>
+                    <li className="header__nav-item">
                         Bank Details 
                     </li>
-                    <li>
+                    <li className="header__nav-item">
                         Loans 
                     </li>
-                    <li>
+                    <li className="header__nav-item">
                         Savings 
                     </li>
-                    <li>
+                    <li className="header__nav-item">
                         App and System
                     </li>
                 </ul>
             </div>
-        </div>
-        <div>
-            <div>
-                <h4>Personal Information</h4>
-                <div>
-                    <span className="">
-                        full name
-                    </span>
-                    <strong className="">
-                        {data.fullname}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        phone number
-                    </span>
-                    <strong className="">
-                        {data.phone_number}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        email address
-                    </span>
-                    <strong className="">
-                        {data.email}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        bvn
-                    </span>
-                    <strong className="">
-                        {data.bvn}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        gender
-                    </span>
-                    <strong className="">
-                        {data.gender}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        marital status
-                    </span>
-                    <strong className="">
-                        {data.marital_status}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        children
-                    </span>
-                    <strong className="">
-                        {data.children}
-                    </strong>
-                </div>
-                <div>
-                    <span className="">
-                        type of residence
-                    </span>
-                    <strong className="">
-                        {data.type_of_residence}
-                    </strong>
                 </div>
             </div>
-            <div>
-                <h4>Education and Employment</h4>
-                <div>
-                    <div>
-                        <span className="">
+        </div>
+        <div className="bio">
+            <div className="bio__info">
+                <h4 className="bio__info-title">Personal Information</h4>
+                <div className="bio__info-details">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            full name
+                        </span>
+                        <strong className="stat__info">
+                            {data.fullname}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            phone number
+                        </span>
+                        <strong className="stat__info">
+                            {data.phone_number}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            email address
+                        </span>
+                        <strong className="stat__info">
+                            {data.email}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            bvn
+                        </span>
+                        <strong className="stat__info">
+                            {data.bvn}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            gender
+                        </span>
+                        <strong className="stat__info">
+                            {data.gender}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            marital status
+                        </span>
+                        <strong className="stat__info">
+                            {data.marital_status}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            children
+                        </span>
+                        <strong className="stat__info">
+                            {data.children}
+                        </strong>
+                    </div>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
+                            type of residence
+                        </span>
+                        <strong className="stat__info">
+                            {data.type_of_residence}
+                        </strong>
+                    </div>
+                </div>
+            </div>
+            <div className="bio__info-2">
+                <h4 className="bio__info-title">Education and Employment</h4>
+                <div className="bio__info-details">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         level of education
                         </span>
-                        <strong className="">
+                        <strong className="stat__info">
                         {data.level_of_education}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         employment status
                         </span>
-                        <strong className="">
+                        <strong className="stat__info">
                         {data.employment_status}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         sector of employment
                         </span>
-                        <strong className="">
+                        <strong className="stat__info">
                         {data.sector_of_employment}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         duration of employment
                         </span>
-                        <strong className="">
+                        <strong className="stat__info">
                         {data.duration_of_employment}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         office email
                         </span>
-                        <strong className="">
+                        <strong className="stat__info">
                         {data.official_email}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         monthly income
                         </span>
-                        <strong className="">
-                        {data.monthly_income_1}-{data.monthly_income_2}
+                        <strong className="stat__info">
+                        ₦{data.monthly_income_1.toLocaleString("en-NG")}-{data.monthly_income_2.toLocaleString("en-NG")}
                         </strong>
                     </div>
-                    <div>
-                        <span className="">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         loan repayment
                         </span>
-                        <strong className="">
-                        ₦{data.loan_repayment}
+                        <strong className="stat__info">
+                        ₦{data.loan_repayment.toLocaleString("en-NG")}
                         </strong>
                     </div>
                 </div>
             </div>
-            <div>
-                <h4>Socials</h4>
-                <div>
-                    <div>
-                        <span>
+            <div className="bio__info-3">
+                <h4 className="bio__info-title">Socials</h4>
+                <div className="bio__info-details">
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         twitter
                         </span>
-                        <strong>
+                        <strong className="stat__info">
                         {data.twitter}
                         </strong>
                     </div>
-                    <div>
-                        <span>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         facebook
                         </span>
-                        <strong>
+                        <strong className="stat__info">
                         {data.facebook}
                         </strong>
                     </div>
-                    <div>
-                        <span>
+                    <div className="details__section-container stat">
+                        <span className="stat__title">
                         instagram
                         </span>
-                        <strong>
+                        <strong className="stat__info">
                         {data.instagram}
                         </strong>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="bio__info-4">
+                <h4 className="bio__info-title">Guarantor</h4>
                 {data.guarantor.map((relative: GuarantorType) => (
-                     <div key={relative.fullname}>
-                        <div>
-                            <span className="">
+                     <div className="bio__info-details" key={relative.fullname}>
+                        <div className="details__section-container stat">
+                            <span className="stat__title">
                             full name
                             </span>
-                            <strong className="">
+                            <strong className="stat__info">
                             {data.fullname}
                             </strong>
                         </div>
-                        <div>
-                            <span className="">
+                        <div className="details__section-container stat">
+                            <span className="stat__title">
                             phone number
                             </span>
-                            <strong className="">
+                            <strong className="stat__info">
                             {relative.phone_number}
                             </strong>
                         </div>
-                        <div>
-                            <span>
+                        <div className="details__section-container stat">
+                            <span className="stat__title">
                             email
                             </span>
-                            <strong className="">
+                            <strong className="stat__info">
                             {relative.email}
                             </strong>
                         </div>
-                        <div>
-                            <span>
+                        <div className="details__section-container stat">
+                            <span className="stat__title">
                                 relationship
                             </span>
-                            <strong>
+                            <strong className="stat__info">
                             {relative.relationship}
                             </strong>
                         </div>
